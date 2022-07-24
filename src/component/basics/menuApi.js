7998199ad0e0238aa1543463620023107330361a
Aliya Huma Ansari <60823426+A-codee@users.noreply.github.com>
2022-07-24 15:15:29 +0530

    Add files via upload

---
+++ src/component/basics/menuApi.js
@@ -0,0 +1,83 @@
+const Menu = [
+{
+    id:1,
+    image: "images/maggi.jpg",
+    name: "maggi",
+    category: "breakfast",
+    price:"12₹",
+    description:"Maggi is cool breakfast. Most recommended breakfast evebody preferes the most.",
+},
+
+{
+    id:2,
+    image: "images/aloopakoda.jpg",
+    name: "aloo pakoda",
+    category: "evening ",
+    price:"20₹",
+    description:"Aloo Pakoda is cool Snack. Most recommended evening snack everybody preferes the most.",
+},
+
+{
+    id:3,
+    image: "images/momos.jpg",
+    name: "momos",
+    category: "breakfast",
+    price:"30₹",
+    description:"Momo is cool breakfast. Most recommended breakfast everybody preferes the most.",
+},
+
+{
+    id:4,
+    image: "images/nonvegthali.jpg",
+    name: "non-veg thali",
+    category: "dinner",
+    price:"150₹",
+    description:"Non Veg thali is special in here.",
+},
+{
+    id:5,
+    image: "images/vegthali.jpg",
+    name: "veg-thali",
+    category: "dinner",
+    price:"100₹",
+    description:"Veg-Thali is special in here",
+},
+{
+    id:6,
+    image: "images/pizza.jpg",
+    name: "pizza",
+    category: "lunch",
+    price:"130₹",
+    description:"Pizza is cool Lunch. Most recommended lunch everybody preferes the most.",
+},
+
+{
+    id:7,
+    image: "images/munchurian.jpg",
+    name: "Munchurian",
+    category: "breakfast",
+    price:"130₹",
+    description:"This is cool snack. Most recommended snack everybody preferes the most.",
+},
+{
+    id:8,
+    image: "images/puri.jpg",
+    name: "Puri",
+    category: "lunch",
+    price:"130₹",
+    description:"puri is cool Lunch. Most recommended lunch everybody preferes the most.",
+},
+{
+    id:9,
+    image: "images/rajmarice.jpg",
+    name: "rajmarice",
+    category: "dinner",
+    price:"130₹",
+    description:"rajmarice is cool Lunch. Most recommended lunch everybody preferes the most.",
+},
+
+
+
+];
+
+export default Menu;
